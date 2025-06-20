07e5efd50b648d020d168be1daf1fb625e1c9245
Katta Sai Durga Prasad <ksaidurga132@gmail.com>
2025-06-20 13:33:36 +0530

    id->product_id

--- products/models.py
+++ products/models.py
@@ -1,14 +1,14 @@
 from django.db import models
 from django.utils import timezone
 class Category(models.Model):
-    id = models.BigAutoField(primary_key=True)
+    Category_id = models.BigAutoField(primary_key=True)
     name = models.CharField(max_length=255, unique=True)
 
     def __str__(self):
         return self.name
 
 class SubCategory(models.Model):
-    id = models.BigAutoField(primary_key=True)
+    SubCategory_id = models.BigAutoField(primary_key=True)
     name = models.CharField(max_length=255)
     category = models.ForeignKey(Category, related_name='subcategories', on_delete=models.CASCADE)
 
@@ -22,7 +22,7 @@ class Product(models.Model):
         ("free", "Free Delivery"),
         ("paid", "Paid Delivery"),
     ]
-    id = models.BigAutoField(primary_key=True)
+    product_id = models.BigAutoField(primary_key=True)
     name = models.CharField(max_length=255)
     rating=models.DecimalField(max_digits=2, decimal_places=1)
     distance = models.DecimalField(max_digits=4, decimal_places=1, blank=True, null=True)
