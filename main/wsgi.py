8fb1c24d342a69bc4ad638542dc17bb2326efc0a
Ranjeet3345 <yadavranjeet7770@gmail.com>
2025-07-25 12:20:38 +0530

    update

--- main/wsgi.py
+++ main/wsgi.py
@@ -6,17 +6,13 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
-import os
-import django
-from django.core.management import call_command
 
-# Set the correct settings module
+import os
+
+from django.core.wsgi import get_wsgi_application
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'main.settings')
 
-# Setup Django and run migrations BEFORE starting the app
-django.setup()
-call_command('migrate')
-
-# Now get the WSGI application
-from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
+
+
