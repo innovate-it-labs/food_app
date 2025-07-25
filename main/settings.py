346def729e411962d5853a8a5953757b5b769ec4
Ranjeet3345 <yadavranjeet7770@gmail.com>
2025-07-25 10:13:10 +0530

    Allowed host updated

--- main/settings.py
+++ main/settings.py
@@ -30,8 +30,11 @@ DEBUG = True
 #     '127.0.0.1',
 #     'foodie-2-l2sv.onrender.com'  # ✅ your Render domain
 # ]
-ALLOWED_HOSTS = ['foodie-2-l2sv.onrender.com', '127.0.0.1', 'localhost']
-
+ALLOWED_HOSTS = [
+    'food-app-isxa.onrender.com',
+    'localhost',
+    '127.0.0.1',
+]
 
 
 
